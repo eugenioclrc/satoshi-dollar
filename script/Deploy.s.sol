f4821c6328d288e6f22dbcacbcb840d2a2d2e00f
eugenioclrc <eugenioclrc@gmail.com>
2025-02-25 14:42:52 -0700

    fixes

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -5,17 +5,17 @@ import {Script, console} from "forge-std/Script.sol";
 import {Manager, SatoshiDollar, IOracle} from "../src/MicroStable.sol";
 
 contract CounterScript is Script {
-    
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        address WBTC = vm.envAddress("WBTC");
-        address oracle = vm.envAddress("oracle");
+        address WBTC = vm.envOr("WBTC", 0xb4255533Ad74A25A83d17154cB48A287E8f6A811);
+        // https://data.chain.link/feeds/ethereum/mainnet/btc-usd
+        address oracle = vm.envOr("oracle", 0xF4030086522a5bEEa4988F8cA5B36dbC97BeE88c);
 
         // Predict manager's address
-        address predictedManagerAddress = vm.computeCreateAddress(address(this), vm.getNonce(address(this)) + 1);
+        address predictedManagerAddress = vm.computeCreateAddress(msg.sender, vm.getNonce(address(this)) + 1);
 
         SatoshiDollar satoshiDollar = new SatoshiDollar(predictedManagerAddress);
         Manager manager = new Manager(address(WBTC), address(satoshiDollar), oracle);
