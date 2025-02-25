eeab2521ce6bd9f3b78746c552da189a46197f5e
eugenioclrc <eugenioclrc@gmail.com>
2025-02-25 14:22:44 -0700

    Deply script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -11,7 +11,16 @@ contract CounterScript is Script {
     function run() public {
         vm.startBroadcast();
 
+        address WBTC = vm.envAddress("WBTC");
+        address oracle = vm.envAddress("oracle");
+
+        // Predict manager's address
+        address predictedManagerAddress = vm.computeCreateAddress(address(this), vm.getNonce(address(this)) + 1);
+
+        SatoshiDollar satoshiDollar = new SatoshiDollar(predictedManagerAddress);
+        Manager manager = new Manager(address(WBTC), address(satoshiDollar), oracle);
 
         vm.stopBroadcast();
+        require(predictedManagerAddress == address(manager), "deployment address error");
     }
 }
